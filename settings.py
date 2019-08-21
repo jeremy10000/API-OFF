261127e9e9202c1c98a63c7069a49c8dc09adbc7
jeremy10000 <jeremy10000@example.com>
2019-08-21 18:25:49 +0200

    add : settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# coding: UTF-8
+
+""" Settings """
+
+# MySQL informations
+
+USERNAME = ""
+PASSWORD = ""
+HOST = ""
+
+# Categories
+
+SEARCH_1 = "Thé"
+SEARCH_2 = "Gâteaux"
+SEARCH_3 = "Céréales"
+SEARCH_4 = "Jus de fruits"
+SEARCH_5 = "Confitures"
+
+# "Advanced" options
+
+DETAILS = True
+
